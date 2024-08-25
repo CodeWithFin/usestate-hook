cf083db054d5d974e33d88aa43f60815daefc95a
Finley <Finley.mwachia12@gmail.com>
2024-08-25 10:38:28 +0300

    successful use of usestate

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you're running this locally in VS Code use the commands:
 // npm install
